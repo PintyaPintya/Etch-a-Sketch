eaf44cc309063a0cad2474336e7a3e95f2c1d2b8
PintyaPintya <omkajale09@gmail.com>
2023-07-19 17:09:37 +0530

    etch a sketch assignment complete

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
 const container = document.getElementById('container')
 
-for(let i = 0; i < 16; i++){
-    for(let j = 0; j < 16; j++){
-        let newDiv = document.createElement('div')
-        newDiv.className = 'box'
-        container.appendChild(newDiv)
+document.querySelector('button').addEventListener('click', userPrompt)
+
+function userPrompt(){
+    let size = 101
+    while(size > 100){
+        size = prompt("Size of the grid?")
+        console.log(size)
     }    
+
+    createGrid(size)
 }
 
-let boxes = document.getElementsByClassName('box')
-boxes = Array.from(boxes)
+function createGrid(size){
+    document.querySelector('#container').style.width = `${24*size+2}px`
+    for(let i = 0; i < size; i++){
+        for(let j = 0; j < size; j++){
+            let newDiv = document.createElement('div')
+            newDiv.className = 'box'
+            container.appendChild(newDiv)
+        }    
+    }
 
-boxes.forEach(box => {
-    box.addEventListener('mouseenter',function(){
-        box.style.backgroundColor = 'red'
-    })
-});
+    let boxes = document.getElementsByClassName('box')
+    boxes = Array.from(boxes)
+    boxes.forEach(box => {
+        box.addEventListener('mouseenter',function(){
+            box.style.backgroundColor = `rgb(${getRandom()},${getRandom()},${getRandom()})`
+        })
+    });
+    
+    // boxes.forEach(box => {
+    //     box.addEventListener('mouseleave',function(){
+    //         box.style.backgroundColor = 'white'
+    //     })
+    // });
+}
 
-boxes.forEach(box => {
-    box.addEventListener('mouseleave',function(){
-        box.style.backgroundColor = 'white'
-    })
-});
+function getRandom(){
+    let n = Math.floor(Math.random() * 255)
+    return n
+}
